3068323395434346175ff17e32aecb2b62651bac
Catherine E Powell <35599212+ceapowell@users.noreply.github.com>
2022-06-14 12:20:54 +0100

    Add files via upload

---
+++ helpme.m
@@ -0,0 +1,17 @@
+% Set up instructions for the Multilevel SGFEM toolbox
+% Multilevel SGFEM scriptfile: CEP; 23 March 2022.
+% Copyright (c) 2022 G. Papanikos, A. J. Crowder, C. E. Powell
+
+%-----------------------Instructions to the users -------------------------
+disp(' ')
+disp('To run the software in MATLAB: ')
+disp(' ')
+disp('1. You should have installed MATLAB version >= 2017b')
+disp('2. If you have installed IFISS, S-IFISS or T-IFISS, remove from current search path')
+disp('3. Change current working directory to ../stoch_diffusion_multilevel')
+disp('4. Change the path in gohome.m to the current working directory') 
+disp('5. Type setpath at command prompt to add folders/files to current path') 
+disp('6. Run driver ML_adapt_SGFEM_pc.m (Windows) or ML_adapt_SGFEM.m (Mac/Linux)') 
+disp('7. Select a test problem and follow instructions on screen.')
+disp(' ')
+%--------------------------------------------------------------------------
